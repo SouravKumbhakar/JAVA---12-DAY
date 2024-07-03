7906025954f6d027d73358ddfce9481fffaa1d00
SouravKumbhakar <souravkumbhakar90@gmail.com>
2024-07-03 21:14:02 +0530

    loop clear➰

---
+++ Day 2/forloop.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class forloop {
+ public static void main(String[] args) {
+    // for(int i=0; i<10 ; i++){
+    //     System.out.println("hello world"); //print hello world 10 times.
+    // }
+            //(1)          (2)        (4)
+    for(int counter =0 ; counter<10; counter++){
+        // System.out.println(counter); //print 0 to 9
+        System.out.print(counter + " "); //print in a same line       (3)
+    }
+ }   
+}
